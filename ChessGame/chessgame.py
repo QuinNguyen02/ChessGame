6c705641415fc3d19aa0e810028902ee96740a9c
Quyen Nguyen <quyen.nguyen@knorex.com>
2024-05-18 10:07:27 +0700

    Add phot & fix music link

--- ChessGame/chessgame.py
+++ ChessGame/chessgame.py
@@ -12,16 +12,16 @@ def main():
         pygame.display.set_caption("Xiangqi Chess")
         surface = pygame.display.get_surface()
         menu = Menu(surface)
-        #setMusic("track3.mp3")
+        setMusic("track3.mp3")
         if menu.play() == 2: # choose 2 players 
-            #setMusic("track2.mp3")
+            setMusic("track2.mp3")
             pygame.mixer.music.stop()
             pygame.display.set_mode((650,600))
             game = twoPlayer(surface,menu)
             game.play()
         elif menu.play() == 1:
             pygame.mixer.music.stop()
-            #setMusic("track1.mp3")
+            setMusic("track1.mp3")
             game = onePlayer(surface)
             game.play() 
         elif menu.play() == 0:
